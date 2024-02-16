2ab4a2de04e546aad65e3bb74aaa0af0070c7954
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2024-02-16 17:37:44 +0100

    init

---
+++ ednevnik/src/main/java/com/iktpreobuka/ednevnik/EdnevnikApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.ednevnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdnevnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdnevnikApplication.class, args);
+	}
+
+}
---
+++ ednevnik/src/test/java/com/iktpreobuka/ednevnik/EdnevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.ednevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdnevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
